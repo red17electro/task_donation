a94c6f8c65547c1acf8662622c58a3e3ee6a9571
Server <red17electro@gmail.com>
2019-02-12 17:40:11 +0100

    fix: prevent default behaviour of the buttons, so the page won't refresh;

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // jQuery onload
 $(document).ready(function() {
   $("#saveForLater").click(function() {
+    e.preventDefault();
     $(this).toggleClass("active");
   });
 
   $("#tellYourFriends").click(function() {
+    e.preventDefault();
     // taken from https://stackoverflow.com/questions/221294/how-do-you-get-a-timestamp-in-javascript
     var timeStampInMs =
       window.performance &&
